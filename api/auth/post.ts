4e526c77123bf391d44ce6d8ec80b333f094ea12
David Vargas <dvargas92495@gmail.com>
2023-07-14 16:00:45 -0400

    Pipe response headers through

--- api/auth/post.ts
+++ api/auth/post.ts
@@ -1,6 +1,12 @@
 import createAPIGatewayHandler from "samepage/backend/createAPIGatewayProxyHandler";
 import axios from "axios";
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST",
+  "Access-Control-Allow-Credentials": true,
+};
+
 export const logic = async ({
   ["x-google-client-id"]: clientId,
   ["x-google-client-secret"]: clientSecret,
@@ -28,7 +34,7 @@ export const logic = async ({
         ? "https://samepage.network/oauth/google"
         : "https://samepage.ngrok.io/oauth/google"),
   };
-  const { data } = await axios
+  const { data: _data } = await axios
     .post<{ access_token: string }>(
       "https://oauth2.googleapis.com/token",
       tokenArgs
@@ -42,12 +48,13 @@ export const logic = async ({
         )
       )
     );
+  const data = { ..._data, headers };
   if (args.grant_type === "authorization_code")
     return axios
       .get(
         `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${data.access_token}`
       )
-      .then((u) => ({ ...data, label: u.data.email }))
+      .then((u) => ({ ...data, label: u.data.email, headers }))
       .catch((e) =>
         Promise.reject(
           new Error(
